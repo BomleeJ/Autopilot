5ad61eadb288586b6e6e37575efdf05a56d312b3
OmarJ <omarkjingo@gmail.com>
2025-12-25 17:39:02 -0500

    Initial Aircraft Guidance Struct

---
+++ include/AircraftGuidance.h
@@ -0,0 +1,34 @@
+#include "Types.h"
+#include <optional>
+/*WHERE WE WANT AIRCRAFT TO GO*/
+
+struct KinematicsTargets {
+    std::optional<FeetPerMinute> vertical_speed_fpm;
+    std::optional<MetersPerSecond> indicated_airspeed_mps;
+};
+
+struct AttitudeTargets {
+    std::optional<Degrees> pitch_deg;
+    std::optional<Degrees> roll_deg;
+    std::optional<Degrees> true_heading_deg;
+    std::optional<Degrees> vpath_pitch;
+};
+
+struct ConfigurationTargets {
+    bool gear_deployed;
+    bool on_ground;
+   std::optional<ControlPosition> flaps_position;
+};
+
+struct PositionTargets {
+    std::optional<Latitude> latitude;
+    std::optional<Longitude> longitude;
+    std::optional<Feet> altitude_msl_ft;
+    std::optional<Feet> altitude_agl_ft;
+};
+struct AircraftGuidance {
+    KinematicsTargets kinematics_targets;
+    AttitudeTargets attitude_targets;
+    ConfigurationTargets configuration_targets;
+    PositionTargets position_targets;
+};
